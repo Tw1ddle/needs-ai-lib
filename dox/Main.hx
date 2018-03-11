8d45ca18b5483bfeb4798eb20f68b723b57eb985
sam <sam@silver42.karoo.co.uk>
2018-03-11 00:30:57 +0000

    Add dox imports

--- dox/Main.hx
+++ dox/Main.hx
@@ -1,10 +1,27 @@
 // This is solely for generating documentation for Needs AI using dox
 
-import lib.needs-ai-lib.needs.ai.Needs;
+import needs.ai.Action;
+import needs.ai.ActionPickingStrategies;
+import needs.ai.ActionScorePair;
+import needs.ai.ActionScoringStrategies;
+import needs.ai.ActionSet;
 import needs.ai.Brain;
-import needs.ai.Brain;
-import needs.ai.Effect;
-import needs.ai.Need;
-import needs.ai.Strategy;
+import needs.ai.Consideration;
+import needs.ai.Reasoner;
+import needs.ai.ReasonerActionPickingStrategies;
+import needs.ai.WatchedBrain;
+import needs.inputs.Input;
+import needs.inputs.DistanceInput;
+import needs.responses.Constant;
+import needs.responses.Linear;
+import needs.responses.Logistic;
+import needs.responses.Logit;
+import needs.responses.Normal;
+import needs.responses.PiecewiseLinear;
+import needs.responses.Polynomial;
+import needs.responses.Response;
+import needs.responses.Sine;
+import needs.util.MathUtil;
+import needs.util.Signal;
 
 class Main {}
