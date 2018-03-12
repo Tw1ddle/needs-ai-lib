c8b355516206ca8d41d37e7064d276e5b182253a
sam <sam@silver42.karoo.co.uk>
2018-03-12 21:42:43 +0000

    ...and some more

--- needs/ai/WatchedBrain.hx
+++ needs/ai/WatchedBrain.hx
@@ -3,7 +3,8 @@ package needs.ai;
 /**
    The WatchedBrain extends the Brain class and keeps track of everything it has ever decided, which is useful for debugging purposes.
 **/
-class WatchedBrain extends Brain<BrainIdType, ReasonerIdType, ActionSetIdType, ActionIdType, ConsiderationIdType, InputIdType> {
+class WatchedBrain<BrainIdType, ReasonerIdType, ActionSetIdType, ActionIdType, ConsiderationIdType, InputIdType>
+extends Brain<BrainIdType, ReasonerIdType, ActionSetIdType, ActionIdType, ConsiderationIdType, InputIdType> {
 	public function new(id:BrainIdType, reasoners:Array<Reasoner<ReasonerIdType, ActionSetIdType, ActionIdType, ConsiderationIdType, InputIdType>>) {
 		super(id, reasoners);
 	}
