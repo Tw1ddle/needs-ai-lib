d672caadb28822a621a78b3a526889a318b81207
sam <sam@silver42.karoo.co.uk>
2018-03-12 21:38:15 +0000

    Add missing type params

--- needs/ai/WatchedBrain.hx
+++ needs/ai/WatchedBrain.hx
@@ -3,7 +3,7 @@ package needs.ai;
 /**
    The WatchedBrain extends the Brain class and keeps track of everything it has ever decided, which is useful for debugging purposes.
 **/
-class WatchedBrain extends Brain {
+class WatchedBrain extends Brain<BrainIdType, ReasonerIdType, ActionSetIdType, ActionIdType, ConsiderationIdType, InputIdType> {
 	public function new(reasoners:Array<Reasoner>) {
 		super(reasoners);
 	}
