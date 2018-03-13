edd8cc76e057abf3a2f0fb65684397ad9d10294c
sam <sam@silver42.karoo.co.uk>
2018-03-13 20:04:44 +0000

    Comment tweak

--- needs/responses/Response.hx
+++ needs/responses/Response.hx
@@ -1,8 +1,10 @@
 package needs.responses;
 
 /**
-   Typedef for a response function type. A response function takes a value (typically normalized into the range 0-1)
-   and returns a transformation of that value, typically also clamped to the range 0-1. Response functions are used to
-   calculate a utility score for a Consideration by processing the value of an Input, which usually originates in the game world.
+   Typedef for the Response function type. A response function takes a value (typically normalized into the range 0-1)
+   and returns a transformation of that value, typically also clamped to the range 0-1. These functions are used to calculate
+   a utility score for a Consideration by processing the value of an Input, which usually originates in the game world.
+   This is response curves in the sense of Bob Alexander in AI Game Programming Wisdom - The Beauty of Response Curves,
+   not a "Best Response" curve in game theory.
 **/
 typedef Response = Float->Float;
